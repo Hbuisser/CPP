a8296df33016a22874f5315c33979c5272a02ad5
Henry Buisseret <henrybuisseret@gmail.com>
2021-04-08 15:06:36 +0200

    ex03 ok

---
+++ DAY03/ex03/FragTrap.hpp
@@ -0,0 +1,31 @@
+
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap(FragTrap const& copy);
+		~FragTrap();
+		FragTrap& operator=(FragTrap const& copy);
+		
+		void getHitPoints();
+		void getEnergyPoints();
+		std::string getName() const;
+
+		void rangedAttack(std::string const& target);
+		void meleeAttack(std::string const& target);
+		void vaulthunter_dot_exe(std::string const & target); 
+
+	private:
+	
+};
+
+#endif
---
+++ DAY03/ex03/NinjaTrap.hpp
@@ -0,0 +1,38 @@
+
+#ifndef NINJATRAP_HPP
+#define NINJATRAP_HPP
+
+# include <iostream>
+# include <string>
+
+# include "ClapTrap.hpp"
+# include "FragTrap.hpp"
+# include "ScavTrap.hpp"
+
+class NinjaTrap : public ClapTrap
+{
+	public:
+		NinjaTrap();
+		NinjaTrap(std::string name);
+		NinjaTrap(NinjaTrap const& copy);
+		~NinjaTrap();
+		NinjaTrap& operator=(NinjaTrap const& copy);
+
+		void getHitPoints();
+		void getEnergyPoints();
+		std::string getName() const;
+		
+		void rangedAttack(std::string const & target);
+		void meleeAttack(std::string const & target);
+
+		void ninjaShoebox(NinjaTrap const &target);
+		void ninjaShoebox(ClapTrap const &target);
+		void ninjaShoebox(FragTrap const &target);
+		void ninjaShoebox(ScavTrap const &target);
+
+	private:
+
+
+};
+
+#endif
