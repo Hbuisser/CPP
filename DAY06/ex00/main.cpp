2ec726b0872f364d2c8d4ab8cb5e1a4f15060d1b
Henry Buisseret <henrybuisseret@gmail.com>
2021-04-21 17:43:39 +0200

    ex00 ok

--- DAY06/ex00/main.cpp
+++ DAY06/ex00/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Data.hpp"
 
 // cast explicit : rajouter le cast avant
@@ -42,14 +43,73 @@ void cast_char(char c)
 void cast_int(int i)
 {
     if (static_cast<char>(i) < 32 || static_cast<char>(i) > 126)
-        std::cout << "Char: non displayable" << std::endl;
+        std::cout << "char: non displayable" << std::endl;
     else
-        std::cout << "Char: " << static_cast<char>(i) << std::endl;
-    std::cout << "Int: " << std::endl;
-    std::cout << "Float: " << static_cast<float>(i) << ".0f" << std::endl;
-    std::cout << "Double: " << static_cast<double>(i) << ".0" << std::endl;
+        std::cout << "char: " << static_cast<char>(i) << std::endl;
+    std::cout << "int: " << std::endl;
+    std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
 }
 
+void cast_double(double d)
+{
+    std::stringstream ss; // https://www.cplusplus.com/reference/sstream/stringstream/stringstream/
+
+	if (static_cast<char>(d) < 32 || static_cast<char>(d) > 126)
+		std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char: " << static_cast<char>(d) << std::endl;
+    std::cout << "int: " << static_cast<int>(d) << std::endl;
+    ss << static_cast<float>(d); // from string to float
+    if (ss.str().find('.') < ss.str().length())
+		std::cout << "float: " << ss.str() << "f" << std::endl;
+	else
+		std::cout << "float: " << ss.str() << ".0f" << std::endl;
+    ss.str(std::string());
+	ss << d;// from string to double
+	if (ss.str().find('.') < ss.str().length())
+		std::cout << "double: " << ss.str() << std::endl;
+	else
+		std::cout << "double: " << ss.str() << ".0" << std::endl;
+}
+
+void special_cast_double(double d)
+{
+    std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
+}
+
+void cast_float(float f)
+{
+	std::stringstream ss;
+
+	if (static_cast<char>(f) < 32 || static_cast<char>(f) > 126)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: " << static_cast<char>(f) << std::endl;
+	std::cout << "int: " << static_cast<int>(f) << std::endl;
+	ss << f;
+	if (ss.str().find('.') < ss.str().length())
+		std::cout << "float: " << ss.str() << "f" << std::endl;
+	else
+		std::cout << "float: " << ss.str() << ".0f" << std::endl;
+	ss.str(std::string());
+	ss << static_cast<double>(f);
+	if (ss.str().find('.') < ss.str().length())
+		std::cout << "double: " << ss.str() << std::endl;
+	else
+		std::cout << "double: " << ss.str() << ".0" << std::endl;
+}
+
+void	special_cast_float(float f)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << static_cast<float>(f) << std::endl;
+}
 
 // Only scalar types, -inff, +inff and nanf are sole accepted pseudo literals
 // Scalar type must belong to int, float or double
@@ -92,6 +152,35 @@ int main(int argc, char **argv)
         return (0);
     }
 
-    
+    // Double ?
+    while (is_nbr(arg[i]))
+        i++;
+    if (arg[i] == '\0')
+    {
+        cast_double(std::stod(arg));
+        return (0);
+    }
+    if (arg == "nan" || arg == "inf" || arg == "-inf")
+	{
+		special_cast_double(std::stod(arg));
+		return 0;
+	}
+
+    // Float
+    if ((arg[i] == 'f' && arg[i + 1] == 0))
+	{
+		cast_float(std::stof(arg));
+		return 0;
+	}
+	if (arg == "nanf" || arg == "inff" || arg == "-inff")
+	{
+		special_from_float(std::stof(arg));
+		return 0;
+	}
+    // Other impossible
+    std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	std::cout << "float: impossible" << std::endl;
+	std::cout << "double: impossible" << std::endl;
     return (0);
 }
