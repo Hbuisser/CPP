be4fa4f3d2267baf2fe3e3d1edca9687c29091eb
Henry Buisseret <henrybuisseret@gmail.com>
2021-04-22 16:01:24 +0200

    day07 ok

--- DAY07/ex02/array.hpp
+++ DAY07/ex02/array.hpp
@@ -13,8 +13,10 @@ class Array
         Array(unsigned int n);
         ~Array(void);
         Array(Array const& other);
+        Array<T>& operator=(Array<T> const& other);
+        T& operator[](unsigned int i);
 
-        unsigned int const getLength() const;
+        unsigned int const size() const;
 
     private:
         unsigned int    m_length;
@@ -39,21 +41,46 @@ Array<T>::~Array(void)
 }
 
 template<typename T>
-Array<T>::Array(Array<T> const& other)
+Array<T>::Array(Array<T> const& copy)
 {
     if (this->m_array)
         delete this->m_array;
-    this->m_array = new T[other.m_length];
-    for (int i = 0; i < other.m_length; i++)
-        m_array[i] = other.m_array[i];
-    m_length = other.m_length;
+    this->m_array = new T[copy.m_length];
+    for (int i = 0; i < copy.m_length; i++)
+        m_array[i] = copy.m_array[i];
+    m_length = copy.m_length;
 }
 
 template<typename T>
-unsigned int const Array<T>::getLength() const
+Array<T>& Array<T>::operator=(Array const& other)
+{
+    if (this != other)
+    {
+        if (m_array)
+            delete (m_array);
+        m_array = new T[other.m_length];
+        for (int i = 0; i < other.m_length; i++)
+            m_array[i] = other.m_array[i];
+        m_length = other.m_length;
+    }
+    return (*this);
+}
+
+template<typename T>
+T& Array<T>::operator[](unsigned int i)
+{
+    if (i > m_length)
+        throw std::exception();
+    return (m_array[i]);
+}
+
+
+template<typename T>
+unsigned int const Array<T>::size() const
 {
     return m_length;
 }
+
 
 
 #endif
--- DAY07/ex02/main.cpp
+++ DAY07/ex02/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include "array.hpp"
 
+class Data
+{
+    public:
+        Data(int n = 88) : m_length(n) {}
+        bool operator==( Data const& other ) { return (this->m_length == other.m_length); }
+        bool operator!=( Data const& other ) { return (this->m_length != other.m_length); }
+        bool operator>( Data const& other ) { return (this->m_length > other.m_length); }
+        bool operator<( Data const& other ) { return (this->m_length < other.m_length); }
+        bool operator>=( Data const& other ) { return (this->m_length >= other.m_length); }
+        bool operator<=( Data const& other ) { return (this->m_length <= other.m_length); }
+        unsigned int m_length;
+};
+
 int main()
 {
     Array<char> a;
     Array<char> b(3);
 
+    b[0] = '1';
+    b[1] = '2';
+    b[2] = '3';
+
+    std::cout << "____________test char__________" << std::endl;
     try
     {
-        std::cout << "Size a: " << a.getLength() << std::endl;
+        std::cout << "Size a: " << a.size() << std::endl;
     }
     catch (const std::exception& e)
     {
@@ -16,11 +34,33 @@ int main()
     }
     try
     {
-        std::cout << "Size b: " << b.getLength() << std::endl;
+        std::cout << "Size b: " << b.size() << std::endl;
+        std::cout << "Elem 7 of b: " << b[6] << std::endl; // pas d'acces donc exception
     }
     catch (const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::cout << e.what() << std::endl;
+    }
+    for (int i = 0; i < b.size(); i++)
+        std::cout << b[i] << std::endl;
+
+    std::cout << "____________test class__________" << std::endl;
+
+    Array<Data> c;
+    Array<Data> d(3);
+
+    d[0] = Data(3);
+    d[1] = Data(42);
+    d[2] = Data(19);
+    try
+    {
+        std::cout << "size c: " << c.size() << std::endl; ; 
+        std::cout << "size d: " << d.size() << std::endl; 
+        d[5] = Data(21);
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
     }
     return (0);
 }
