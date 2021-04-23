5576e2cb6705a31a713bd936d42695b31e4026a6
Henry Buisseret <henrybuisseret@gmail.com>
2021-04-23 10:08:43 +0200

    day08 begin

---
+++ DAY08/ex00/easyfind.hpp
@@ -0,0 +1,14 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+template<typename T>
+const int easyfind(const T &array, int n)
+{
+    
+}
+
+# endif
---
+++ DAY08/ex00/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "easyfind.hpp"
+
+// stl : standard template library
+
+// containers : 
+// std::list<int>   lst1;
+// std::map<>
+// std::vector
+
+// lst1.push_back(42);
+
+// iterator : (equivalent stl d'un pointeur)
+// std::list<int>::const_iterator   i;
+// std::list<int>::const_iterator   i_end = lest1.end();
+
+// for (i = lst1.begin(); i != i_end; ++i)
+//        std::cout << *i << std::endl;
+
+
